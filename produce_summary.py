0a6fa55765932fb1255209c0079ec320b4d4603f
Nicolas Drygas <nicolas.drygas@live.com>
2023-04-06 23:08:45 -0700

    Created function for future sales

--- produce_summary.py
+++ produce_summary.py
@@ -1,4 +1,20 @@
-print("Day 1")
+
+def daily_report(filename):
+    """Takes in a file and prints the sales summary for that day"""
+    the_file = open(filename)
+    for line in the_file:
+        words = line.rstrip().split("|")
+        # words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+daily_report("um-deliveries-day-1.txt")
+
+"""print("Day 1")
 the_file = open("um-deliveries-day-1.txt")
 for line in the_file:
     line = line.rstrip()
@@ -11,7 +27,7 @@ for line in the_file:
     print(f"Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
 
-
+#old methods for reference
 print("Day 2")
 the_file = open("um-deliveries-day-2.txt")
 for line in the_file:
@@ -23,10 +39,10 @@ for line in the_file:
     amount = words[0]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+the_file.close()"""
 
 
-print("Day 3")
+"""print("Day 3")
 the_file = open("um-deliveries-day-3.txt")
 for line in the_file:
     line = line.rstrip()
@@ -37,4 +53,4 @@ for line in the_file:
     amount = words[0]
 
     print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+the_file.close()"""
